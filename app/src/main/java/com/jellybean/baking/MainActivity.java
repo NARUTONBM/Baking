757e6852e34f5b0676379ad5a11824955a220b28
jellybean <narutonbm@gmail.com>
2018-01-06 21:03:53 +0800

    Init

---
+++ app/src/main/java/com/jellybean/baking/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jellybean.baking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author jellybean
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
