8451672d629acf35bcf03b4cc2439bb86723fd5e
jellybean <narutonbm@gmail.com>
2018-02-01 00:34:01 +0800

    done with recipe item

--- app/src/main/java/com/jellybean/baking/adapter/RecipeItemAdapter.java
+++ app/src/main/java/com/jellybean/baking/adapter/RecipeItemAdapter.java
@@ -5,11 +5,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jellybean.baking.MyApplication;
 import com.jellybean.baking.R;
 import com.jellybean.baking.bean.RecipeBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,11 +24,16 @@ public class RecipeItemAdapter extends RecyclerView.Adapter {
 
 	private Context mContext = new MyApplication().getContext();
 	List<RecipeBean> mRecipeBeanList;
+	List<Integer> mNameList = new ArrayList<Integer>();
 
 	@Override
 	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
 		View view = LayoutInflater.from(mContext).inflate(R.layout.recipe_item, parent, false);
+		mNameList.add(0, R.mipmap.cover_nutella_pie);
+		mNameList.add(1, R.mipmap.cover_brownies);
+		mNameList.add(2, R.mipmap.cover_yellow_cake);
+		mNameList.add(3, R.mipmap.cover_cheesecake);
 
 		return new RecipeViewHolder(view);
 	}
@@ -33,6 +41,11 @@ public class RecipeItemAdapter extends RecyclerView.Adapter {
 	@Override
 	public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
+		RecipeViewHolder recipeViewHolder = (RecipeViewHolder) holder;
+		RecipeBean recipeBean = mRecipeBeanList.get(position);
+		recipeViewHolder.mIvCover.setImageResource(mNameList.get(position));
+		recipeViewHolder.mTvRecipeName.setText(recipeBean.getName());
+		recipeViewHolder.mTvRecipeStep.setText(recipeBean.getSteps().size());
 	}
 
 	@Override
@@ -43,9 +56,16 @@ public class RecipeItemAdapter extends RecyclerView.Adapter {
 
 	private class RecipeViewHolder extends RecyclerView.ViewHolder {
 
+		private final ImageView mIvCover;
+		private final TextView mTvRecipeName;
+		private final TextView mTvRecipeStep;
+
 		public RecipeViewHolder(View view) {
 
 			super(view);
+			mIvCover = view.findViewById(R.id.iv_cover);
+			mTvRecipeName = view.findViewById(R.id.tv_recipe_name);
+			mTvRecipeStep = view.findViewById(R.id.tv_recipe_step);
 		}
 	}
 }
